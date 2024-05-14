783b89d4001c087a72d806ab25d227870871abd8
Mikhail <ummsion@gmail.com>
2024-05-14 19:23:12 +0300

    add to commit Intermediate

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/expertGit/expertGitGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef EXPERTGIT_expertGitGameModeBase_generated_h
+#error "expertGitGameModeBase.generated.h already included, missing '#pragma once' in expertGitGameModeBase.h"
+#endif
+#define EXPERTGIT_expertGitGameModeBase_generated_h
+
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_SPARSE_DATA
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_RPC_WRAPPERS
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAexpertGitGameModeBase(); \
+	friend struct Z_Construct_UClass_AexpertGitGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AexpertGitGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/expertGit"), NO_API) \
+	DECLARE_SERIALIZER(AexpertGitGameModeBase)
+
+
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAexpertGitGameModeBase(); \
+	friend struct Z_Construct_UClass_AexpertGitGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AexpertGitGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/expertGit"), NO_API) \
+	DECLARE_SERIALIZER(AexpertGitGameModeBase)
+
+
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AexpertGitGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AexpertGitGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AexpertGitGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AexpertGitGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AexpertGitGameModeBase(AexpertGitGameModeBase&&); \
+	NO_API AexpertGitGameModeBase(const AexpertGitGameModeBase&); \
+public:
+
+
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AexpertGitGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AexpertGitGameModeBase(AexpertGitGameModeBase&&); \
+	NO_API AexpertGitGameModeBase(const AexpertGitGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AexpertGitGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AexpertGitGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AexpertGitGameModeBase)
+
+
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_12_PROLOG
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_SPARSE_DATA \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_RPC_WRAPPERS \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_INCLASS \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define expertGit_Source_expertGit_expertGitGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_SPARSE_DATA \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	expertGit_Source_expertGit_expertGitGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> EXPERTGIT_API UClass* StaticClass<class AexpertGitGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID expertGit_Source_expertGit_expertGitGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Intermediate/Build/Win64/expertGitEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.39.33523
+#include "E:/git/expert/expertGit/Intermediate/Build/Win64/expertGitEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
